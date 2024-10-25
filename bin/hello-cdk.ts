44f4a03513f5d4a8f8dbd873f3a1f720221a29b8
Nate Wiger <nwiger@gmail.com>
2024-10-24 17:20:03 -0700

    progress

--- bin/hello-cdk.ts
+++ bin/hello-cdk.ts
@@ -3,6 +3,9 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { HelloCdkStack } from '../lib/hello-cdk-stack';
 
+const CDK_DEFAULT_ACCOUNT = '920372993539';
+const CDK_DEFAULT_REGION = 'us-west-2';
+
 const app = new cdk.App();
 new HelloCdkStack(app, 'HelloCdkStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
@@ -15,7 +18,7 @@ new HelloCdkStack(app, 'HelloCdkStack', {
 
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+  env: { account: CDK_DEFAULT_ACCOUNT, region: CDK_DEFAULT_REGION },
 
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+// Import the Lambda module
+import { Code, Function, Runtime, FunctionUrlAuthType } from 'aws-cdk-lib/aws-lambda';
+import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +15,27 @@ export class HelloCdkStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    // Define the Lambda function resource
+    const myFunction = new Function(this, "HelloWorldFunction", {
+      runtime: Runtime.NODEJS_20_X, // Provide any supported Node.js runtime
+      code: Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
+    })
+
+    // Define the Lambda function URL resource
+    const myFunctionUrl = myFunction.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+    });
+
+    // Define a CloudFormation output for your URL
+    new cdk.CfnOutput(this, "myFunctionUrlOutput", {
+      value: myFunctionUrl.url,
+    })
+
+    // Define the API Gateway resource
+    const gateway = new LambdaRestApi(this, "HelloWorldGateway", {
+      handler: myFunction
+    });
   }
 }
